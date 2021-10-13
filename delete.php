24783b1ee713a927ccb4dece7e9f87d0918bd9b4
yaelx <yaelx@localhost>
2021-10-13 12:53:57 +0200

    update name variable

--- delete.php
+++ delete.php
@@ -1,11 +1,11 @@
 <?php
-    // BDD File
-    require "database.php";
+// BDD File
+require "database.php";
 
-    $id = $_GET['id'];
-    $sql = "DELETE fROM `argonautes` WHERE id=:id";
-    $res = $db_connect->prepare($sql);
-    
-    if ($res->execute([':id' => $id])) {
-        header("Location: /challengeWild/");
-      }
+$id = $_GET['id'];
+$sql = "DELETE fROM `argonautes` WHERE id=:id";
+$res = $db_connect->prepare($sql);
+
+if ($res->execute([':id' => $id])) {
+    header("Location: /challengeWild/");
+}
--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 // header
-require "header.php"; ?>
+require "header.php";?>
 <!-- Main section -->
   <main>
-    <?php require "database.php"; ?>
+    <?php require "database.php";?>
 
     <!-- New member form -->
     <h2>Ajouter un(e) Argonaute</h2>
@@ -14,62 +14,60 @@ require "header.php"; ?>
     </form>
 
     <?php
-        if (isset($_POST['nameArg'])){
-            // Récupérer les valeurs 
-            $nameArg = $_POST['nameArg'];
-            // Requête mysql pour insérer des données
-            $sql = "INSERT INTO `argonautes`(`name`) VALUES (:nameArg)";
-            // On prépare la requête sql
-            $res = $db_connect->prepare($sql);
-            // On exécute la requête sql
-            $exec = $res->execute(array(":nameArg"=>$nameArg,));
+if (isset($_POST['nameArg'])) {
+    // Récupérer les valeurs
+    $nameArg = $_POST['nameArg'];
+    // Requête mysql pour insérer des données
+    $sql = "INSERT INTO `argonautes`(`name`) VALUES (:nameArg)";
+    // On prépare la requête sql
+    $res = $db_connect->prepare($sql);
+    // On exécute la requête sql
+    $exec = $res->execute(array(":nameArg" => $nameArg));
 
-            // vérifier si la requête d'insertion a réussi   
-            if($exec){
-            echo '<div class="argonaute_response--box"><p class="argonaute_response--valid">l\'argonaute a bien rejoint l\'équipage !!</p></div>';
-            }else{
-            echo '<div class="argonaute_response--box"><p class="argonaute_response--fail">L\'argonautes n\'a pas pu embarquer avec le reste de l\'équipage</p></div>';
-            }
-        }
-    ?>
-    
+    // vérifier si la requête d'insertion a réussi
+    if ($exec) {
+        echo '<div class="argonaute_response--box"><p class="argonaute_response--valid">l\'argonaute a bien rejoint l\'équipage !!</p></div>';
+    } else {
+        echo '<div class="argonaute_response--box"><p class="argonaute_response--fail">L\'argonautes n\'a pas pu embarquer avec le reste de l\'équipage</p></div>';
+    }
+}
+?>
+
     <!-- Member list -->
     <h2>Membres de l'équipage</h2>
     <?php
 
-    // Afficher les valeurs de la base de donnée
-        $sql = "SELECT * FROM argonautes";
-        $res_select = $db_connect->query($sql);
-        $resLine = $res_select->fetch();
+// Afficher les valeurs de la base de donnée
+$sql = "SELECT * FROM argonautes";
+$fetchQuery = $db_connect->query($sql);
+$fetchData = $fetchQuery->fetch();
 
+if ($fetchData === false) {
+    echo '<h3>Il n\'y a aucun nom!</h3>';
+} else {
+    // On affiche chaque entrée une à une
 
-        if($resLine === false) {
-            echo '<h3>Il n\'y a aucun nom!</h3>';
-        }
-        else {
-            // On affiche chaque entrée une à une
-            
-        do {
-            ?>
+    do {
+        ?>
                 <section class="member-list">
                     <div class="member-item">
                         <img class="member-icon" src="img/helmet.png">
-                        <p><?php echo $resLine['name']; ?></p>
-                        <a href="delete.php?id=<?= $resLine["id"]; ?>">
+                        <p><?php echo $fetchData['name']; ?></p>
+                        <a href="delete.php?id=<?=$fetchData["id"];?>">
                             <img class="member-icon" src="img/bin.png" alt="poubelle">
                         </a>
                     </div>
                 </section>
             <?php
-            } while ($resLine = $res_select->fetch());
-            $resLine = "";
-        }
-        
-        ?>
+} while ($fetchData = $fetchQuery->fetch());
+    $fetchData = "";
+}
+
+?>
 
   </main>
-  
+
   <?php
 
 // footer
-require "footer.php"; ?>
+require "footer.php";?>
